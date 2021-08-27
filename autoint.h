bc0228381426eaed4828560adac3511809b63367
talbii <talbioffical@gmail.com>
2021-08-27 23:32:31 +0300

    Improved header file comments

--- autoint.h
+++ autoint.h
@@ -17,20 +17,37 @@ struct AutoInt {
 };
 
 /* Memory Functions */
-struct AutoInt* autoint_create(void);                      // allocate a new empty AutoInt. Will return NULL if malloc() fails.
-void autoint_safecreate(struct AutoInt* preallocated);           // given a preallocated AutoInt, this will fill the struct with default values.
-int deleteNumber(struct AutoInt* x);                     // frees an AutoInt
+
+/** allocate a new empty AutoInt. Will return NULL if malloc() fails. */
+struct AutoInt* autoint_create(void);
+
+/** given a preallocated AutoInt, this will fill the struct with default values. */
+void autoint_safecreate(struct AutoInt* preallocated);
+
+/** frees an AutoInt */
+int deleteNumber(struct AutoInt* x);
+
 /* Arithmetic Functions  */
-struct AutoInt  autoint_add(struct AutoInt x, struct AutoInt y); // adds two AutoInts       (x + y)
-struct AutoInt  autoint_sub(struct AutoInt x, struct AutoInt y); // Subtracts two AutoInts  (x - y)
-struct AutoInt  autoint_mul(struct AutoInt x, struct AutoInt y); // multiplies two AutoInts (x * y)
-struct AutoInt  autoint_div(struct AutoInt x, struct AutoInt y); // divides two AutoInts    (x / y)
-struct AutoInt  autoint_mod(struct AutoInt x, struct AutoInt y); // returns the remainder   (x % y)
+
+struct AutoInt  autoint_add(struct AutoInt x, struct AutoInt y); 
+struct AutoInt  autoint_sub(struct AutoInt x, struct AutoInt y); 
+struct AutoInt  autoint_mul(struct AutoInt x, struct AutoInt y); 
+struct AutoInt  autoint_div(struct AutoInt x, struct AutoInt y); 
+struct AutoInt  autoint_mod(struct AutoInt x, struct AutoInt y); 
+
 /* IO Functions */
-void autoint_print(struct AutoInt* x);                     // prints an AutoInt, safely
-char* autoint_to_string(struct AutoInt* x);                      // returns the string representing the given AutoInt. Note, it is the caller's responsibility to free() that string later.
+
+/** prints an AutoInt, safely (does not use malloc) */
+void autoint_print(struct AutoInt* x);
+
+/** returns the string representing the given AutoInt. Note, it is the caller's responsibility to free() that string later. (this function uses malloc!)*/
+char* autoint_to_string(struct AutoInt* x);
 /* Misc. Functions */
-struct AutoInt ToAutoInt(long long int n);               // returns an AutoInt from the given number n
-struct AutoInt fast_ToAutoInt(signed char n);            // statically creates an AutoInt from a given number in [-128, 127].
+
+/** returns an AutoInt from the given number n */
+struct AutoInt ToAutoInt(long long int n);
+
+/** statically creates an AutoInt from a given number in [-128, 127]. */
+struct AutoInt fast_ToAutoInt(signed char n);
 
 #endif // __AUTOINT_H
