ac166b730e29c9a00d6ec1526d3b004f9a71007b
talbii <talbioffical@gmail.com>
2021-08-27 23:26:15 +0300

    Renamed AutoInt functions to avoid conflicts

--- autoint.h
+++ autoint.h
@@ -12,23 +12,23 @@ struct autoint__digit {
 struct AutoInt {
     int carry : 1;        // carry flag
     int positive : 1;     // is_positive
-    struct __digit* data;
+    struct autoint__digit* data;
     int len;
 };
 
 /* Memory Functions */
-struct AutoInt* createNumber(void);                      // allocate a new empty AutoInt. Will return NULL if malloc() fails.
-void safeCreate(struct AutoInt* preallocated);           // given a preallocated AutoInt, this will fill the struct with default values.
+struct AutoInt* autoint_create(void);                      // allocate a new empty AutoInt. Will return NULL if malloc() fails.
+void autoint_safecreate(struct AutoInt* preallocated);           // given a preallocated AutoInt, this will fill the struct with default values.
 int deleteNumber(struct AutoInt* x);                     // frees an AutoInt
 /* Arithmetic Functions  */
-struct AutoInt  add(struct AutoInt x, struct AutoInt y); // adds two AutoInts       (x + y)
-struct AutoInt  sub(struct AutoInt x, struct AutoInt y); // Subtracts two AutoInts  (x - y)
-struct AutoInt  mul(struct AutoInt x, struct AutoInt y); // multiplies two AutoInts (x * y)
-struct AutoInt  div(struct AutoInt x, struct AutoInt y); // divides two AutoInts    (x / y)
-struct AutoInt  mod(struct AutoInt x, struct AutoInt y); // returns the remainder   (x % y)
+struct AutoInt  autoint_add(struct AutoInt x, struct AutoInt y); // adds two AutoInts       (x + y)
+struct AutoInt  autoint_sub(struct AutoInt x, struct AutoInt y); // Subtracts two AutoInts  (x - y)
+struct AutoInt  autoint_mul(struct AutoInt x, struct AutoInt y); // multiplies two AutoInts (x * y)
+struct AutoInt  autoint_div(struct AutoInt x, struct AutoInt y); // divides two AutoInts    (x / y)
+struct AutoInt  autoint_mod(struct AutoInt x, struct AutoInt y); // returns the remainder   (x % y)
 /* IO Functions */
-void printNumber(struct AutoInt* x);                     // prints an AutoInt, safely
-char* to_string(struct AutoInt* x);                      // returns the string representing the given AutoInt. Note, it is the caller's responsibility to free() that string later.
+void autoint_print(struct AutoInt* x);                     // prints an AutoInt, safely
+char* autoint_to_string(struct AutoInt* x);                      // returns the string representing the given AutoInt. Note, it is the caller's responsibility to free() that string later.
 /* Misc. Functions */
 struct AutoInt ToAutoInt(long long int n);               // returns an AutoInt from the given number n
 struct AutoInt fast_ToAutoInt(signed char n);            // statically creates an AutoInt from a given number in [-128, 127].
